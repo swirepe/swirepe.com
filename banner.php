c5a17ba8dc6d05f9880d5ef577a2a69b2fc7a954
Peter Swire <swirepe@gmail.com>
2012-04-27 19:11:24 -0400

    started the move to php

---
+++ banner.php
@@ -0,0 +1,10 @@
+<div class="banner-panel">
+
+   <ul class="banner-panel">
+        <li class="banner-button"><a class="banner-button" href="/about">About</a></li>
+        <li class="banner-button" ><a class="banner-button" href="/contact" id="contact">Contact</a></li>
+        <li class="banner-button"><a class="banner-button" href="/projects">Projects</a></li>
+        <li class="banner-button"><a class="banner-button" href="/cv">CV</a></li>
+	</ul>
+    <div class="stripe"></div>
+</div>
---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>About Peter Swire</title>
+	<link rel="stylesheet" type="text/css" href="/main.css" />
+</head>
+<body>
+        <div class="body-container">
+        	<?php include("banner.php"); ?>
+        
+
+	        <div class="name">Peter<br/>Swire</div>
+
+	        <div class="subname">[ <i>machine learning &amp; data mining specialist</i> ] </div>
+
+		</div>
+</body>
+</html>
