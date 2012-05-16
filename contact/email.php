fdf9cbb3b0fd69d675f28efe6857c2f1f4c123b7
Peter Swire <swirepe@gmail.com>
2012-05-16 02:12:31 -0400

    contact (minus gol) is done

---
+++ contact/email.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" type="text/css" href="box.css" />
+<div class="box">
+	<p style="font-size:80px;">swirepe at gmail dot com</p>
+	<p style="font-size:35px; margin-top:-45px">(high volume)</p>
+</div>
+
+
+<div class="box" style="margin-top:-30px; background-color:#0754a0">
+	<p style="font-size:55px;">the dot victoriant at gmail dot com</p>
+	<p style="font-size:20px; margin-top:-45px">(low volume)</p>
+</div>
