797153b1be8c5e4a87e57ac3c15a6a3efa56330d
Peter Swire <swirepe@gmail.com>
2012-05-15 23:14:51 -0400

    now a banner on the sites page

---
+++ include/swirepe.php
@@ -0,0 +1,5 @@
+<h1 id="swirepe.com">swirepe.com</h1>
+<h2 id="youre-looking-at-it">you're looking at it</h2>
+<p>This site is in a git repository!</p>
+<p>Watch me learn php in front of a live studio audience.</p>
+<p><a href="https://github.com/swirepe/swirepe.com">github</a></p>
---
+++ projects/sites/site-banner.php
@@ -0,0 +1,62 @@
+<style type="text/css">
+
+div.site-banner-panel{
+	background-color: #3AA6D0;
+	width:100%;
+	margin-bottom:50px;
+	margin-left:0;
+	margin-right:auto;
+	text-align:center;
+    right:0px;
+
+    position:absolute;
+    top: 65px;
+}
+
+ul.site-banner-panel {
+    list-style-type:none;
+    margin:0 auto;
+    padding:0px;
+    width:854px;
+}
+
+li.site-button {
+    float:left;
+}
+
+a.site-button{
+    padding:0px;
+    margin:0px;
+    text-align:center;
+    color:white;
+    font-size:15px;
+    font-weight:bold;
+    line-height:30px;
+    display:block;
+    width:122px;
+    text-decoration:none;
+}
+
+
+a.site-button:hover{
+    background-color:#024C68;
+}
+
+a.site-button:visited{
+    color:white;
+}
+
+</style>
+
+<div class="site-banner-panel">
+	<ul class="site-banner-panel">
+		<li class="site-button"><a class="site-button" href="/">Home</a></li>
+		<li class="site-button"><a class="site-button" href="?project=drfiveminutes">DRFVMN</a></li>
+		<li class="site-button"><a class="site-button" href="?project=important4batia">Important4batia</a></li>
+		<li class="site-button"><a class="site-button" href="?project=victoriant">Victoriant</a></li>
+		<li class="site-button"><a class="site-button" href="?project=tireless">Tireless</a></li>
+		<li class="site-button"><a class="site-button" href="?project=inappropriamatic">Inappropriamatic</a></li>
+		<li class="site-button"><a class="site-button" href="?project=swirepe">swirepe.com</a></li>
+
+	</ul>
+</div>
