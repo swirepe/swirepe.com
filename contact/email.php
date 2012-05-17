103f472daa18fc5512ff387037307d580c9b45c4
Peter Swire <swirepe@gmail.com>
2012-05-16 23:23:16 -0400

    added grey bits to the contact emails

--- contact/email.php
+++ contact/email.php
@@ -6,6 +6,6 @@
 
 
 <div class="box" style="margin-top:-30px; background-color:#0754a0">
-	<p style="font-size:55px;">the <span class="grey">dot</span> victoriant <span class="grey">at</span> gmail <span class="grey">dot</span> com</p>
+	<p style="font-size:55px;">the <span class="darkgrey">dot</span> victoriant <span class="darkgrey">at</span> gmail <span class="darkgrey">dot</span> com</p>
 	<p style="font-size:20px; margin-top:-45px">(low volume)</p>
 </div>
