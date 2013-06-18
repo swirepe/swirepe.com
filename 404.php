5d143cfc65fea4e25275841389d4cd11134f8585
Peter Swire <swirepe@gmail.com>
2013-06-18 16:26:50 -0400

    added a blank 404

---
+++ 404.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Peter Swire - 404</title>
+    <link rel="stylesheet" type="text/css" href="/main.css" />
+	<?php include($_SERVER['DOCUMENT_ROOT']."/include/styleh1.php"); ?>
+
+</head>
+<body>
+    <div class="body-container">
+        <?php include($_SERVER['DOCUMENT_ROOT']."/include/banner.php"); ?>
+
+    </div>
+</body>
+</html>
