acacf790d1465dee3d966bfbd2d2c0b67a71169a
Peter Swire <swirepe@gmail.com>
2013-06-19 11:17:36 -0400

    made a nice 404 page with toilet

--- 404.php
+++ 404.php
@@ -9,6 +9,48 @@
     <div class="body-container">
         <?php include($_SERVER['DOCUMENT_ROOT']."/include/banner.php"); ?>
 
+
+<style type="text/css">
+.forohfour {
+    width:800px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 100px;
+    font-size: 60px;
+}
+
+.forohforexplanation{
+    text-align:center;
+    width:300px;
+    margin-left: auto;
+    margin-right: auto;
+    
+}
+.forohforexplanation p {
+    font-size:20px;
+    color: white;
+    font-style:bold;
+}
+
+</style>
+
+        
+<div class="forohfour">
+<div style="font-family: monospace, fixed; font-weight: bold;">
+<span style="">&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;</span><br />
+<span style="">&#160;&#160;&#160;&#160;</span><span style=";color:#5f5">mm</span><span style="">&#160;&#160;&#160;</span><span style=";color:#55f">m</span><span style=";color:#f5f">mm</span><span style=";color:#f55">m</span><span style="">&#160;&#160;&#160;&#160;&#160;</span><span style=";color:#5ff">mm</span><span style="">&#160;</span><br />
+<span style="">&#160;&#160;&#160;</span><span style=";color:#5f5">m</span><span style=";color:#5ff">&quot;#</span><span style="">&#160;&#160;</span><span style=";color:#f5f">m&quot;</span><span style="">&#160;&#160;</span><span style=";color:#ff5">&quot;m</span><span style="">&#160;&#160;&#160;</span><span style=";color:#5ff">m</span><span style=";color:#55f">&quot;#</span><span style="">&#160;</span><br />
+<span style="">&#160;&#160;</span><span style=";color:#5ff">#&quot;</span><span style="">&#160;</span><span style=";color:#55f">#</span><span style="">&#160;&#160;</span><span style=";color:#f55">#</span><span style="">&#160;&#160;</span><span style=";color:#ff5">m</span><span style="">&#160;</span><span style=";color:#5f5">#</span><span style="">&#160;&#160;</span><span style=";color:#55f">#&quot;</span><span style="">&#160;</span><span style=";color:#f5f">#</span><span style="">&#160;</span><br />
+<span style="">&#160;</span><span style=";color:#5ff">#</span><span style=";color:#55f">mm</span><span style=";color:#f5f">m#</span><span style=";color:#f55">m</span><span style="">&#160;</span><span style=";color:#ff5">#</span><span style="">&#160;&#160;&#160;&#160;</span><span style=";color:#5ff">#</span><span style="">&#160;</span><span style=";color:#55f">#</span><span style=";color:#f5f">mm</span><span style=";color:#f55">m#</span><span style=";color:#ff5">m</span><br />
+<span style="">&#160;&#160;&#160;&#160;&#160;</span><span style=";color:#f55">#</span><span style="">&#160;&#160;&#160;</span><span style=";color:#5f5">#</span><span style=";color:#5ff">mm</span><span style=";color:#55f">#</span><span style="">&#160;&#160;&#160;&#160;&#160;&#160;</span><span style=";color:#ff5">#</span><span style="">&#160;</span><br />
+<span style="">&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;</span><br />
+<span style="">&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;</span><br />
+</div>
+
+<div class="forohforexplanation">
+<p>Page not found.</p>
+</div>
+        
     </div>
 </body>
 </html>
