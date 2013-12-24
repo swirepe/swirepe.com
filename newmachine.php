d43d06ba49f93e50f9bd9ed8b1238f33a99f294e
Peter Swire <swirepe@gmail.com>
2013-12-24 00:39:34 -0500

    added an extension and some formatting to newmachine command

---
+++ newmachine.php
@@ -0,0 +1,8 @@
+<pre>wget https://raw.github.com/swirepe/personalscripts/master/setup-new-machine.sh && chmod +x setup-new-machine.sh && ./setup-new-machine.sh | tee new-machine-setup.log 
+</pre>
+
+<br><br><br>
+
+<pre>
+curl -L https://raw.github.com/swirepe/personalscripts/master/setup-new-machine.sh | bash
+</pre>
