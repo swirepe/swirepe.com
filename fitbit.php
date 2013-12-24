2c4da62cc508c65abdd6ea2122c8eb7997ef5f06
Peter Swire <swirepe@gmail.com>
2013-12-24 00:12:56 -0500

    added a page for mom's christmas gift

---
+++ fitbit.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+
+<h1>Fitbit En Route!</h1>
+
+<p>Dear Mom,</p>
+
+<p>Your fitbit is on the way!  They had some trouble getting them all out of the door in time for Christmas, but it should be here soon.</p>  
+
+<p><a href="https://www.fitbit.com/story">This is what it is.</a></p>
+
+<p>Here is the tracking for it:</p>
+
+<iframe width="100%" height="1000" src="https://tools.usps.com/go/TrackConfirmAction_input?qtc_tLabels1=9205561199137304655720">
+
+
+
+
+</html>
